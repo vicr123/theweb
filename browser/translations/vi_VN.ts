c68a8f850ababdd4cf3255445e456a3409b45cf1
Victor Tran <vicr12345@gmail.com>
2019-08-14 16:12:32 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 89.5% (51 of 57 strings)
    
    Translation: theWeb/theWeb Desktop
    Translate-URL: https://translate.vicr123.com/projects/theweb/theweb-desktop/vi/

--- browser/translations/vi_VN.ts
+++ browser/translations/vi_VN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>CertificateErrorPane</name>
     <message>
@@ -11,82 +11,82 @@
     <message>
         <location filename="../tab/certificateerrorpane.ui" line="40"/>
         <source>You find yourself in a strange place</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn tìm chính bạn trong một nơi kỳ cục</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.ui" line="76"/>
         <source>The site owner asked us to ensure that the connection is always secure, so at this point in time, you cannot continue to this page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Người quản lý trang web này đã yêu cầu chúng tôi để xác nhận kết nối an toàn; vì vậy bạn không có thể tiếp tục tới trang web này bây giờ.</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.ui" line="110"/>
         <source>What&apos;s wrong?</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuyện gì vậy?</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.ui" line="123"/>
         <source>Go Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Trở lại</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.ui" line="202"/>
         <source>CONTINUE ANYWAY</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếp tục</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.ui" line="234"/>
         <source>Continue to the site</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếp tục đến trang web này</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="49"/>
         <source>Looks like we can&apos;t get to the real %1. For your security, we&apos;ve stopped this connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Có vẻ như chúng tôi không thể kết nối với %1 thật. Vì an toàn của bạn, chúng tôi đã ngưng kết nối này.</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="54"/>
         <source>Only continue to this page if you understand the risks involved. Don&apos;t enter any sensitive info, because attackers may be watching.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nên bạn chỉ tiếp tục nếu bạn hiểu những rủi ro liên quan. Đừng nhập thing tin cá nhân, bởi vì kẻ tấn công có thể nhìn thấy.</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="56"/>
         <source>This page enforces HSTS, so you may not continue to this page at this time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Trang web này thực thi Bảo Mật Truyền Tải Nghiêm Ngặt HTTP (HSTS); vì vậy bạn không có thể tiếp tục tới trang web này bây giờ.</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="65"/>
         <source>presented us with an expired certificate</source>
-        <translation type="unfinished"></translation>
+        <translation>cung cấp cho chúng tôi với một chứng chỉ đã hết hạn</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="68"/>
         <source>presented us with a certificate for another website</source>
-        <translation type="unfinished"></translation>
+        <translation>cung cấp cho chúng tôi với một chứng chỉ cho trang web khác</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="72"/>
         <source>presented us with an invalid certificate</source>
-        <translation type="unfinished"></translation>
+        <translation>cung cấp cho chúng tôi với một chứng chỉ không hợp lệ</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="75"/>
         <source>presented us with a revoked certificate</source>
-        <translation type="unfinished"></translation>
+        <translation>cung cấp cho chúng tôi với một chứng chỉ đã bị thu hồi</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="76"/>
         <source>The site presented a revoked certificate, so you may not continue to this page at this time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Trang web này cung cấp cho chúng tôi với một chứng chỉ đã bị thu hồi; vì vậy bạn không có thể tiếp tục tới trang web này bây giờ.</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="80"/>
         <source>presented us with an untrusted certificate; the issuer of this certificate is not in your system&apos;s CA store</source>
-        <translation type="unfinished"></translation>
+        <translation>cung cấp cho chúng tôi với một chứng chỉ hệ thống không tin tưởng; tổ chức phát hành chứng chỉ này không có tìm thấy trong kho chứng chỉ hệ thống của bạn</translation>
     </message>
     <message>
         <location filename="../tab/certificateerrorpane.cpp" line="85"/>
         <source>We weren&apos;t able to verify that you&apos;re connecting to the real %1 because it %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi không thể xác minh bạn đang kết nối với %1 thật, vì nó %2.</translation>
     </message>
 </context>
 <context>
@@ -99,7 +99,7 @@
     <message>
         <location filename="../popovers/jsalert.ui" line="51"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Báo</translation>
     </message>
 </context>
 <context>
@@ -112,7 +112,7 @@
     <message>
         <location filename="../popovers/jsconfirm.ui" line="71"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Báo</translation>
     </message>
 </context>
 <context>
@@ -120,62 +120,62 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>theWeb</source>
-        <translation type="unfinished"></translation>
+        <translation>theWeb</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Tệp</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="100"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Sửa</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="105"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="132"/>
         <source>Go Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Trở lại</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="135"/>
         <source>Alt+Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+Left</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
         <source>Go Forward</source>
-        <translation type="unfinished"></translation>
+        <translation>Bước tiếp</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="146"/>
         <source>Alt+Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+Right</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="154"/>
         <source>Reload</source>
-        <translation type="unfinished"></translation>
+        <translation>Tải lại</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="157"/>
         <source>Ctrl+R</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+R</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="165"/>
         <source>New Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Tab mới</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="168"/>
         <source>Ctrl+T</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+T</translation>
     </message>
 </context>
 <context>
@@ -188,92 +188,92 @@
     <message>
         <location filename="../tab/permissionpopup.ui" line="108"/>
         <source>Allow</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.ui" line="115"/>
         <source>Deny</source>
-        <translation type="unfinished"></translation>
+        <translation>Từ chối</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="41"/>
         <source>Full Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Toàn màn hình</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="42"/>
         <source>%1 is now full screen.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 đang hiện toàn màn hình.</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="47"/>
         <source>Geolocation</source>
-        <translation type="unfinished"></translation>
+        <translation>Vị trí</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="48"/>
         <source>Allow %1 to access your physical location?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 truy cập vị trí thực của bạn?</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="52"/>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy ảnh</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="53"/>
         <source>Allow %1 to access your camera?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 truy cập vào máy ảnh của bạn?</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="57"/>
         <source>Microphone</source>
-        <translation type="unfinished"></translation>
+        <translation>Micrô</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="58"/>
         <source>Allow %1 to access your microphone?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 truy cập vào micrô của bạn?</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="62"/>
         <source>Camera and Microphone</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy ảnh và micrô</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="63"/>
         <source>Allow %1 to access your camera and microphone?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 truy cập vào máy ảnh và micrô của bạn?</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="68"/>
         <source>Screen Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>Thâu màn hình</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="69"/>
         <source>Allow %1 to record your screen?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 thâu màn hình của bạn?</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="73"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông báo</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="74"/>
         <source>Allow %1 to send notifications?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 gửi tin nhắn?</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="78"/>
         <source>Mouse Control</source>
-        <translation type="unfinished"></translation>
+        <translation>Điều khiển chuột</translation>
     </message>
     <message>
         <location filename="../tab/permissionpopup.cpp" line="79"/>
         <source>Allow %1 to take exclusive control of your mouse?</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho phép %1 kiểm soát độc quyền chuột của bạn?</translation>
     </message>
 </context>
 <context>
@@ -286,12 +286,12 @@
     <message>
         <location filename="../widgets/securitychunk.cpp" line="136"/>
         <source>Generated by theWeb</source>
-        <translation type="unfinished"></translation>
+        <translation>Được tạo ra bởi theWeb</translation>
     </message>
     <message>
         <location filename="../widgets/securitychunk.cpp" line="141"/>
         <source>Insecure</source>
-        <translation type="unfinished"></translation>
+        <translation>Không bảo mật</translation>
     </message>
 </context>
 <context>
@@ -307,7 +307,7 @@
     <message>
         <location filename="../main.cpp" line="43"/>
         <source>Web Browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Trình duyệt web</translation>
     </message>
 </context>
 </TS>
