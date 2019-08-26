6e59a6cc52106c55fd0c0371a2dc6467d023e413
Victor Tran <vicr12345@gmail.com>
2019-08-27 01:10:21 +1000

    Remove debug ls

--- ci/travis.sh
+++ ci/travis.sh
@@ -100,7 +100,6 @@ elif [ $STAGE = "after_success" ]; then
   if [ $TRAVIS_OS_NAME = "linux" ]; then
     echo "[TRAVIS] Publishing AppImage"
     wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
-    ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
     cp theWeb*.AppImage theWeb-linux.AppImage
     cp theWeb*.AppImage.zsync theWeb-linux.AppImage.zsync
     bash upload.sh theWeb-linux.AppImage*
